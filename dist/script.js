6414bed44bc99cf23b4528b4bf49fc18194cbd74
HuiyaoHu <huiyaohu@gmail.com>
2021-08-29 22:31:54 +0800

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,8 @@
 // View website by typing in PS C:\Users\huiya\Desktop\3jsProject\threejs-webpack-starter> npm run dev
 
 import './style.css'
-import * as THREE from 'https://unpkg.com/three/build/three.module.js'          // Import the entire three.js core library
-import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'https://cdn.skypack.dev/three'          // Import the entire three.js core library
+import { OrbitControls } from 'https://cdn.skypack.dev/three@<version>/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'          // control panel GUI
 
 // TextureLoader
